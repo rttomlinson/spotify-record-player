23163d82e6b0b9d3e8452320b9fcd772d843d4e4
Renzo Tomlinson <rttomlinson@gmail.com>
2020-10-20 21:02:47 -0600

    More docs

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-uses sqlite3
-We just need some kind of remote data store. We could actually just use a file.And is completely reasonable since the data should never change.
-you can also put the data in a database if you want a little exposure to sql databases. I have the code commented out if you want to do that
-But this is fine exposure to a simple database :) and super easy to use and install
-* sudo apt-get install sqlite3
-
-https://github.com/spotify/web-api-auth-examples
-
 Environment variables:
 DATA_FILE - file path to json data
-requires
-client_id
-client_secret
-device_id
-access_token
-refresh_token
+
+You need to update the /etc/default/raspotify file to use. This values are simply passed to the librespot
+Optionally: 
+DEVICE_NAME="raspotify" (How the device shows up in the menu)
+Required:
+OPTIONS="--username <spotify_username> --password <spotify_password>"
+
+username and password should be for a dedicated spotify account and not used in ANY of your other accounts. The reason for this is that this data is stored in plaintext and one should not rely on raspberry pi default settings for securely storing data on the device. If someone wants to implement some kind of secret store for these values then go for it. 
+
+requires in json
+client_id: string
+client_secret: string
+device_id: string
+access_token: not actually required (could be optimized)
+refresh_token: string
 
 can use the python .env library
 
@@ -26,3 +27,11 @@ you also need a dedicated spotify account since we reinitize the raspotify/libre
 Initialization can also happen by fetching the device ID based on device name which you also set.
 
 you need to acquire an access token and refresh token using the authorization grant
+https://github.com/spotify/web-api-auth-examples
+
+uses sqlite3
+We just need some kind of remote data store. We could actually just use a file.And is completely reasonable since the data should never change.
+you can also put the data in a database if you want a little exposure to sql databases. I have the code commented out if you want to do that
+But this is fine exposure to a simple database :) and super easy to use and install
+* sudo apt-get install sqlite3
+
--- barcode_scanner_video.py
+++ barcode_scanner_video.py
@@ -7,7 +7,7 @@ import imutils
 import time
 import cv2
 import requests
-import sqlite3
+#import sqlite3
 import subprocess
 import base64
 import os
@@ -26,18 +26,20 @@ load_dotenv()
 
 #def get_access_token():
     # We always assume that id = 1 is the only user and it gets reset each time. Would be cool to do some IOT stuff here.
-#    c.execute("""SELECT * FROM users WHERE id=1""")
+    #c.execute("""SELECT * FROM users WHERE id=1""")
     # Should return a tuple in the form (id, access_token, refresh_token)
-#    data = c.fetchone()
-#    return data[1]
+    # data = c.fetchone()
+    # return data[1]
 
 def use_refresh_token_for_new_access_token(access_credentials):
+    ### sqlite3 code
     # Get refresh token
     # We always assume that id = 1 is the only user and it gets reset each time. Would be cool to do some IOT stuff here.
-#    c.execute("""SELECT * FROM users WHERE id=1""")
+    #c.execute("""SELECT * FROM users WHERE id=1""")
     # Should return a tuple in the form (id, access_token, refresh_token)
-#    data = c.fetchone()
-#    refresh_token = data[2] # refresh_token in position 2
+    #data = c.fetchone()
+    #refresh_token = data[2] # refresh_token in position 2
+    
     # Get new access token
     url = "https://accounts.spotify.com/api/token"
     b64encoded_data = base64.b64encode((access_credentials["client_id"] + ":" + access_credentials["client_secret"]).encode("utf-8")).decode("utf-8")
@@ -45,13 +47,12 @@ def use_refresh_token_for_new_access_token(access_credentials):
     r = requests.post(url, headers=headers, data={"grant_type": "refresh_token", "refresh_token": access_credentials["refresh_token"]})
 
     token_data = r.json()
-    # Update the table
-#    c.execute("""UPDATE users SET access_token = ? WHERE id=1""", [token_data["access_token"]])
-#    conn.commit()
-    # Return new access token
 
-    # Return the updated data object
-    #access_credentials["access_token"] = token_data["access_token"]
+    # Update the table if you want to fetch from the table
+    # c.execute("""UPDATE users SET access_token = ? WHERE id=1""", [token_data["access_token"]])
+    # conn.commit()
+
+    # Return the access token
     return token_data["access_token"]
 
 def make_song_request(access_credentials, uri):
@@ -65,13 +66,11 @@ def make_song_request(access_credentials, uri):
 
     headers = {"Authorization": "Bearer " + access_token}
     
-    # Do we store the device ID? Probably
     # Always cast to device raspberry pi device (This will kick whoever is on off)
     # TODO figure out how to manage this device id
     raspotify_device_id = access_credentials["device_id"]
     # How to get device id? Does it change or always the same??
-    r_transfer_playback = requests.put("https://api.spotify.com/v1/me/player", headers=headers,
-            json={"device_ids":[raspotify_device_id], "play": True})
+    r_transfer_playback = requests.put("https://api.spotify.com/v1/me/player", headers=headers, json={"device_ids":[raspotify_device_id], "play": True})
     # Always turn shuffle off
     r_shuffle = requests.put("https://api.spotify.com/v1/me/player/shuffle?state=false", headers=headers)
     
@@ -102,8 +101,6 @@ def start_video_stream(access_credentials, frames_pause_threshold = 6, time_betw
     vs = VideoStream(usePiCamera=True).start()
     print("video should be streaming")
     time.sleep(2.0)
-
-    # open the output CSV file for writing and initialize the set of barcodes found so far
 
     # loop over the frames from the video stream
     while True:
@@ -155,8 +152,6 @@ if __name__ == "__main__":
         with open(data_file_path) as in_file:
             access_credentials = json.load(in_file)
             start_video_stream(access_credentials)
-        # Or fetch from data
-
         # Or do something else, doesn't really matter for this app. Fetch from a secrets manager
 
     finally:
